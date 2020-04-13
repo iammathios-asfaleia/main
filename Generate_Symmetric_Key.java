c18801aeb572fe338da726ed9c2ea252ea37d4e2
DennisJo <dionisistzo@gmail.com>
2020-04-13 21:56:23 +0300

    Add files via upload

--- Generate_Symmetric_Key.java
+++ Generate_Symmetric_Key.java
@@ -9,11 +9,12 @@ public class Generate_Symmetric_Key {
     public static SecretKey createSymmetricalKey() {
         KeyGenerator keyGenerator;
         try {
+            // δημιουργια κλειδιου οπου θα εχει μεγεθος 256byte
             keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(256);
             SecretKey secretKey = keyGenerator.generateKey();
-            byte[] aeskey = secretKey.getEncoded();
-            SecretKey secret_key = new SecretKeySpec(aeskey, "AES");
+            byte[] aeskey = secretKey.getEncoded(); // περασμα κλειδιου σε πινακα απο byte[]
+            SecretKey secret_key = new SecretKeySpec(aeskey, "AES"); // δημιουργια μυστικου κλειδιου 
             return secret_key;
         }catch (NoSuchAlgorithmException ex){
             ex.printStackTrace();
@@ -21,23 +22,25 @@ public class Generate_Symmetric_Key {
         return null;
     }
 
+    // Κρυπτογραφιση κλειδιου
     public static byte[] encrypt(byte[] value,SecretKey secret_key) {
         try{
-            Cipher aescipher = Cipher.getInstance("AES");;
-            aescipher.init(Cipher.ENCRYPT_MODE,secret_key);
-            return aescipher.doFinal(value);
+            Cipher aescipher = Cipher.getInstance("AES"); // Δημιουργια αντικειμενου cipher οπου χρησιμοποιει τον αλγοριθμο AES 
+            aescipher.init(Cipher.ENCRYPT_MODE,secret_key); // κρυπτογραφηση κλειδιου 
+            return aescipher.doFinal(value); // επιστρεφομενη τιμη το κειμενο οπου κρυπτογραφειται 
         }catch (InvalidKeyException|BadPaddingException|IllegalBlockSizeException|NoSuchPaddingException|NoSuchAlgorithmException exception){
             exception.printStackTrace();
         }
         return null;
     }
 
+    // Απο-κρυπτογραφιση κλειδιου
     public static byte[] decrypt(byte[] encryption_value,SecretKey secret_key) {
 
         try{
             Cipher aescipher = Cipher.getInstance("AES");
-            aescipher.init(Cipher.DECRYPT_MODE,secret_key);
-            return aescipher.doFinal(encryption_value);
+            aescipher.init(Cipher.DECRYPT_MODE,secret_key); // απο-κρυπτογραφηση κλειδιου 
+            return aescipher.doFinal(encryption_value);// επιστρεφομενη τιμη η απο-κρυπτογραφηση του κρυπτογραφημενου κειμενου  
         }catch (InvalidKeyException|BadPaddingException|IllegalBlockSizeException|NoSuchPaddingException|NoSuchAlgorithmException exception){
             exception.printStackTrace();
         }
